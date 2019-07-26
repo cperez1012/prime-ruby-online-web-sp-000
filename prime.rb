e5942aaa4cc0db1c26e75c27609ab4d944c08f56
Christian Perez <perezchristian1012@gmail.com>
2019-07-26 21:20:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(array)
   if array < 0 || array == 0 || array == 1
     return false
-  else (2..number-1).each { |int| return false if number % int == 0}
+  else (2..array-1).each { |int| return false if array % int == 0}
  true
 end
 end
